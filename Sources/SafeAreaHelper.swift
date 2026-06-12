6a01c6f86d4b0dec82e9d05ce80c6cc328b960aa
MXFia19 <130248051+MXFia19@users.noreply.github.com>
2026-06-12 05:21:53 +0200

    Update SafeAreaHelper.swift

--- Sources/SafeAreaHelper.swift
+++ Sources/SafeAreaHelper.swift
@@ -2,12 +2,12 @@ import UIKit
 
 extension UIApplication {
     static var safeAreaTop: CGFloat {
-        connectedScenes
+        UIApplication.shared.connectedScenes
             .compactMap { $0 as? UIWindowScene }
             .first?.windows.first(where: { $0.isKeyWindow })?.safeAreaInsets.top ?? 47
     }
     static var safeAreaBottom: CGFloat {
-        connectedScenes
+        UIApplication.shared.connectedScenes
             .compactMap { $0 as? UIWindowScene }
             .first?.windows.first(where: { $0.isKeyWindow })?.safeAreaInsets.bottom ?? 34
     }
